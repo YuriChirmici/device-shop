110a4c8511318308a46d5b78d13676d53d692c0d
Yuri Kirmichi <ykirmichi4@mail.ru>
2023-08-13 02:25:18 +0300

    fill user controller

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,15 +1,79 @@
-class DeviceController {
-	async create(req, res) {
+const path = require("path");
+const uuid = require("uuid").v4;
+const { Device, DeviceInfo } = require("../database/models");
 
+class DeviceController {
+	async create(req, res, next) {
+		try {
+			const { name, price, brandId, typeId } = req.body;
+			let { info } = req.body;
+			const { img } = req.files;
+			let filename = uuid() + ".jpg";
+			await img.mv(path.resolve(__dirname, "../static", filename));
+
+			const device = await Device.create({ name, price, brandId, typeId, img: filename });
+			if (info) {
+				info = (typeof info === "string") ? JSON.parse(info) : info;
+				await deviceController._createDeviceInfo(info, device.id);
+			}
+
+			res.json(device);
+		} catch (err) {
+			next(err);
+		}
 	}
 
 	async getAll(req, res) {
+		let { limit = 9, page = 1 } = req.query;
+		let offset = (page - 1) * limit;
 
+		limit = limit < 1 ? 1 : limit;
+		offset = offset < 0 ? 0 : offset;
+
+		const query = {};
+		["brandId", "typeId"].forEach((key) => {
+			const value = req.query[key];
+			if (value) {
+				query[key] = value;
+			}
+		})
+
+		const devices = await Device.findAndCountAll({ where: query, limit, offset })
+		res.json(devices);
 	}
 
-	async getById(req, res) {
+	async getById(req, res, next) {
+		try {
+			const { id } = req.params;
+			const device = await Device.findOne({
+				where: { id },
+				include: [{ model: DeviceInfo, as: "info" }]
+			})
+			res.json(device);
+		} catch (err) {
+			next(err);
+		}
+	}
 
+	async _createDeviceInfo(info = [], deviceId) {
+		if (!info.length) {
+			return;
+		}
+
+		const promises = [];
+		info.forEach((i) => {
+			promises.push(DeviceInfo.create({
+				title: i.title,
+				description: i.description,
+				deviceId
+			}))
+		})
+
+		await Promise.all(promises);
 	}
 }
 
-module.exports = new DeviceController();
+
+const deviceController = new DeviceController();
+
+module.exports = deviceController;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const sequelize = require("./database/db");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 const router = require("./routes");
+const sequelize = require("./database/db");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload());
 app.use("/api", router);
 
 const start = async () => {
