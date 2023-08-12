acc4e0d3235b9f05772207929821d463b93c1885
Yuri Kirmichi <ykirmichi4@mail.ru>
2023-08-13 00:29:31 +0300

    add error handler

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,12 @@ const brandRouter = require("./brandRouter");
 const deviceRouter = require("./deviceRouter");
 const typeRouter = require("./typeRouter");
 const userRouter = require("./userRouter");
+const ErrorHandler = require("../middleware/ErrorHandler");
 
 router.use("/brand", brandRouter);
 router.use("/device", deviceRouter);
 router.use("/type", typeRouter);
 router.use("/user", userRouter);
 
+router.use(ErrorHandler);
 module.exports = router;
