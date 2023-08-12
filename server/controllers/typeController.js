45cb747db754df0de6ad6f84cba6c3a952036a9c
Yuri Kirmichi <ykirmichi4@mail.ru>
2023-08-13 01:15:00 +0300

    fill type and brand controllers

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,10 +1,24 @@
-class TypeController {
-	async create(req, res) {
+const { Type } = require("../database/models");
+const ApiError = require("../error/ApiError");
 
+class TypeController {
+	async create(req, res, next) {
+		try {
+			const { name } = req.body;
+			const type = await Type.create({ name });
+			res.json(type);
+		} catch (err) {
+			next(err);
+		}
 	}
 
 	async getAll(req, res) {
-
+		try {
+			const types = await Type.findAll();
+			res.json(types);
+		} catch (err) {
+			next(err);
+		}
 	}
 }
 
