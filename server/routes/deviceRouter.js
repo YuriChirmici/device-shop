87a5cb23877b67c17db047290b9024059dcc8122
Yuri Kirmichi <ykirmichi4@mail.ru>
2023-08-13 14:31:28 +0300

    add registration and permissions

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const deviceController = require("../controllers/deviceController");
+const authMiddleware = require("../middleware/auth");
 
-router.post("/", deviceController.create);
+router.post("/", authMiddleware(["ADMIN"]), deviceController.create);
 router.get("/", deviceController.getAll);
 router.get("/:id", deviceController.getById);
 
