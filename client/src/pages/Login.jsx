0858eb8eb25396be8143c009263c2a5dbca6d324
Yuri Kirmichi <ykirmichi4@mail.ru>
2023-08-13 18:45:45 +0300

    add login and registration pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
+import { REGISTRATION_ROUTE } from '../utils/consts';
 
 const Login = () => {
 	return (
-		<div>
-			Login
-		</div>
+		<Container className="d-flex justify-content-center align-items-center">
+			<Card style={{ width: 600 }} className="p-5 mt-3">
+				<h2 className='m-auto'> Login </h2>
+				<Form className="d-flex flex-column">
+					<Form.Control
+						className="mt-3"
+						placeholder="Enter you email..."
+					/>
+					<Form.Control
+						className="mt-3"
+						placeholder="Enter you password..."
+					/>
+					<div className='d-flex justify-content-between align-items-center mt-3'>
+						<div>
+							<span> Don't have an account yet? </span>
+							<NavLink to={REGISTRATION_ROUTE}> Sign Up </NavLink>
+						</div>
+						<Button variant='outline-success'>
+							Login
+						</Button>
+					</div>
+					
+				</Form>
+			</Card>
+		</Container>
 	);
 };
 
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
 
 const Registration = () => {
 	return (
-		<div>
-			Registration
-		</div>
+		<Container className="d-flex justify-content-center align-items-center">
+			<Card style={{ width: 600 }} className="p-5 mt-3">
+				<h2 className='m-auto'> Registration </h2>
+				<Form className="d-flex flex-column">
+					<Form.Control
+						className="mt-3"
+						placeholder="Enter you email..."
+					/>
+					<Form.Control
+						className="mt-3"
+						placeholder="Enter you password..."
+					/>
+					<div className='d-flex justify-content-between align-items-center mt-3'>
+						<div>
+							<span> Already have an account? </span>
+							<NavLink to={LOGIN_ROUTE}> Login </NavLink>
+						</div>
+						<Button variant='outline-success'>
+							Sign Up
+						</Button>
+					</div>
+					
+				</Form>
+			</Card>
+		</Container>
 	);
 };
 
